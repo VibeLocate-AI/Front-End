d7d3c3918bc9bf3f319b220097ccaa69e7307e9b
AnasAshour20 <111207557+AnasAshour20@users.noreply.github.com>
2026-09-22 14:31:59 +0300

    about

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -27,7 +27,7 @@
           <router-link class="nav-item" to="/new-projects">{{ t('newProjects') }}</router-link>
           <router-link class="nav-item" to="/map">{{ t('interactiveMap') }} <i class="fa-solid fa-map-location-dot" style="font-size:0.75rem; color:var(--accent-cyan); margin-left:3px;"></i></router-link>
           <a class="nav-item" href="#areas" @click.prevent="scrollTo('areas')">{{ t('areas') }}</a>
-          <a class="nav-item" href="#about" @click.prevent="scrollTo('about')">{{ t('aboutUs') }}</a>
+          <router-link class="nav-item" to="/about">{{ t('aboutUs') }}</router-link>
         </nav>
 
         <!-- Header Actions -->
